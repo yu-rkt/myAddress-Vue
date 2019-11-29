6c38f9b02e337849e59a8a1ddc0fd5e25eb08606
watanabeyu <y.watanabe.rkt@gmail.com>
2019-11-29 15:07:18 +0900

    Vue.jsの練習(マイアドレス帳)

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="toggleSideMenu"></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>マイアドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <SideNav></SideNav>
+      <router-view></router-view>
+    </v-content>
+
+
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SideNav from './components/SideNav'
+import { mapActions } from 'vuex'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data () {
+    return {
+      //
     }
+  },
+  methods: {
+    ...mapActions(['toggleSideMenu'])
   }
 }
-</style>
+</script>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout wrap style="height: 200px;">
+
+    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+      <v-list class="pa-1">
+        <v-list-item avatar>
+          <v-list-item-avatar>
+            <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Kazuya Kojima</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-item v-for="item in items" :key="item.title" >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { title: '連絡先一覧', icon: 'list' }
+      ]
+    }
+  }
+}
+</script>
